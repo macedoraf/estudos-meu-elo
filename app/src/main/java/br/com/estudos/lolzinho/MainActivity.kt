68e6db79c58caf17f2961a2f935c047a9906a63b
Rafael Macedo <macedo.rafanazareth@gmail.com>
2022-04-29 10:17:50 -0300

    Primeira tela feita

--- app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
+++ app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
@@ -1,11 +1,21 @@
 package br.com.estudos.lolzinho
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnEntrar : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnEntrar = findViewById(R.id.bt_perfil)
+
+        btnEntrar.setOnClickListener(View.OnClickListener {
+            val myIntent = Intent(this, PerfilActivity::class.java)
+            this.startActivity(myIntent)
+        })
     }
 }
---
+++ app/src/main/java/br/com/estudos/lolzinho/PerfilActivity.kt
@@ -0,0 +1,11 @@
+package br.com.estudos.lolzinho
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
