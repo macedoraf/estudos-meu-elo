d006cf570e5ce091681ad9b3bf811ba861d6a86f
Rafael Macedo <macedo.rafanazareth@gmail.com>
2022-04-29 12:10:34 -0300

    Primeira tela e segunda tela finalizada

--- app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
+++ app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnEntrar : Button
+
+    lateinit var btnEntrar: Button
+    lateinit var etInvocador: TextView
+    lateinit var etElo: TextView
+    lateinit var etDivisao: TextView
+    lateinit var etPdl: TextView
+    lateinit var etCampeao: TextView
+    lateinit var etVitorias: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnEntrar = findViewById(R.id.bt_perfil)
+        etInvocador = findViewById(R.id.et_invocador)
+        etElo = findViewById(R.id.et_elo)
+        etDivisao = findViewById(R.id.et_divisao)
+        etPdl = findViewById(R.id.et_pdl)
+        etCampeao = findViewById(R.id.et_campeao)
+        etVitorias = findViewById(R.id.et_vitorias)
 
         btnEntrar.setOnClickListener(View.OnClickListener {
             val myIntent = Intent(this, PerfilActivity::class.java)
+            myIntent.putExtra("invocador", etInvocador.text.toString())
+            myIntent.putExtra("elo", etElo.text.toString())
+            myIntent.putExtra("divisao", etDivisao.text.toString())
+            myIntent.putExtra("pdl", etPdl.text.toString())
+            myIntent.putExtra("campeao", etCampeao.text.toString())
+            myIntent.putExtra("vitorias", etVitorias.text.toString())
             this.startActivity(myIntent)
         })
     }
--- app/src/main/java/br/com/estudos/lolzinho/PerfilActivity.kt
+++ app/src/main/java/br/com/estudos/lolzinho/PerfilActivity.kt
@@ -1,11 +1,32 @@
 package br.com.estudos.lolzinho
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class PerfilActivity : AppCompatActivity() {
+    lateinit var invocador: TextView
+    lateinit var elo : TextView
+    lateinit var divisao : TextView
+    lateinit var pdl : TextView
+    lateinit var campeao: TextView
+    lateinit var vitorias : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+        invocador = findViewById(R.id.tv_invocador)
+        elo = findViewById(R.id.tv_elo)
+        divisao = findViewById(R.id.tv_divisao)
+        pdl = findViewById(R.id.tv_pdl)
+        campeao = findViewById(R.id.tv_campeao)
+        vitorias = findViewById(R.id.tv_vitorias)
+
+        invocador.text = intent.extras?.getString("invocador")
+        elo.text = intent.extras?.getString("elo")
+        divisao.text = intent.extras?.getString("divisao")
+        pdl.text = intent.extras?.getString("pdl")
+        campeao.text = intent.extras?.getString("campeao")
+        vitorias.text = intent.extras?.getString("vitorias")
     }
 }
