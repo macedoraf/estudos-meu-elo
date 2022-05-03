656dc0e8984c9ddc45b15a9bd8214b6d6a5dc789
Victorio <vpn.cash@gmail.com>
2022-05-03 10:48:27 -0300

    Tela de perfil atualizada

--- app/src/main/java/br/com/estudos/lolzinho/PerfilActivity.kt
+++ app/src/main/java/br/com/estudos/lolzinho/PerfilActivity.kt
@@ -1,32 +1,62 @@
 package br.com.estudos.lolzinho
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class PerfilActivity : AppCompatActivity() {
     lateinit var invocador: TextView
-    lateinit var elo : TextView
-    lateinit var divisao : TextView
-    lateinit var pdl : TextView
+    lateinit var divisao: TextView
+    lateinit var pdl: TextView
     lateinit var campeao: TextView
-    lateinit var vitorias : TextView
+    lateinit var vitorias: TextView
+    lateinit var emblema: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
         invocador = findViewById(R.id.tv_invocador)
-        elo = findViewById(R.id.tv_elo)
         divisao = findViewById(R.id.tv_divisao)
         pdl = findViewById(R.id.tv_pdl)
         campeao = findViewById(R.id.tv_campeao)
         vitorias = findViewById(R.id.tv_vitorias)
+        emblema = findViewById(R.id.img_elo)
 
         invocador.text = intent.extras?.getString("invocador")
-        elo.text = intent.extras?.getString("elo")
-        divisao.text = intent.extras?.getString("divisao")
         pdl.text = intent.extras?.getString("pdl")
+        divisao.text = intent.extras?.getString("divisao")
         campeao.text = intent.extras?.getString("campeao")
-        vitorias.text = intent.extras?.getString("vitorias")
+        var percentVitorias = intent.extras?.getInt("vitorias") ?: 0
+        var eloDesk = intent.extras?.getString("elo")
+
+        if (percentVitorias > 50) {
+            vitorias.setTextColor(resources.getColor(R.color.green))
+        } else if (percentVitorias < 50) {
+            vitorias.setTextColor(resources.getColor(R.color.red))
+        } else {
+            vitorias.setTextColor(resources.getColor(R.color.gray))
+        }
+        vitorias.text = "${percentVitorias}%"
+
+        if (eloDesk == "ferro" || eloDesk == "Ferro") {
+            emblema.setImageResource(R.drawable.emblem_ferro)
+        } else if (eloDesk == "bronze" || eloDesk == "Bronze") {
+            emblema.setImageResource(R.drawable.emblem_bronze)
+        } else if (eloDesk == "prata" || eloDesk == "Prata") {
+            emblema.setImageResource(R.drawable.emblem_silver)
+        } else if (eloDesk == "ouro" || eloDesk == "Ouro") {
+            emblema.setImageResource(R.drawable.emblem_gold)
+        } else if (eloDesk == "platina" || eloDesk == "Platina") {
+            emblema.setImageResource(R.drawable.emblem_platinum)
+        } else if (eloDesk == "diamante" || eloDesk == "Diamante") {
+            emblema.setImageResource(R.drawable.emblem_diamond)
+        } else if (eloDesk == "mestre" || eloDesk == "Mestre") {
+            emblema.setImageResource(R.drawable.emblem_master)
+        } else if (eloDesk == "grao mestre" || eloDesk == "Grao Mestre") {
+            emblema.setImageResource(R.drawable.emblem_grandmaster)
+        } else if (eloDesk == "desafiante" || eloDesk == "Desafiante") {
+            emblema.setImageResource(R.drawable.emblem_challenger)
+        }
     }
 }
