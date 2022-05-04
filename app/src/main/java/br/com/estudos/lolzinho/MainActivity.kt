2eb5e21b18eca4f1a3b94f018bc51245545c4505
Victorio <vpn.cash@gmail.com>
2022-05-04 12:05:02 -0300

    Tela de Login/v3.1 finalizada

--- app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
+++ app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
@@ -1,11 +1,13 @@
 package br.com.estudos.lolzinho
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var etPdl: TextView
     lateinit var etCampeao: TextView
     lateinit var etVitorias: TextView
+    var textoDeErro: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,14 +32,71 @@ class MainActivity : AppCompatActivity() {
         etVitorias = findViewById(R.id.et_vitorias)
 
         btnEntrar.setOnClickListener(View.OnClickListener {
-            val myIntent = Intent(this, PerfilActivity::class.java)
-            myIntent.putExtra("invocador", etInvocador.text.toString())
-            myIntent.putExtra("elo", etElo.text.toString())
-            myIntent.putExtra("divisao", etDivisao.text.toString())
-            myIntent.putExtra("pdl", etPdl.text.toString())
-            myIntent.putExtra("campeao", etCampeao.text.toString())
-            myIntent.putExtra("vitorias", etVitorias.text.toString().toInt())
-            this.startActivity(myIntent)
+            if (osCamposSaoValidos()) {
+                navegaParaOutraTela()
+            } else {
+                Toast.makeText(this.baseContext, "$textoDeErro", Toast.LENGTH_LONG).show()
+            }
         })
+    }
+
+    fun adicionaTexto(param:String){
+        if (textoDeErro == ""){
+            textoDeErro += param
+        } else {
+            textoDeErro += "\n" + param
+        }
+    }
+
+    fun osCamposSaoValidos(): Boolean {
+
+        textoDeErro = ""
+        var nomeDoInvocador = etInvocador.text.toString()
+        var elo = etElo.text.toString()
+        var divisao = etDivisao.text.toString()
+        var pdl = etPdl.text.toString()
+        var campeao = etCampeao.text.toString()
+        var vitorias = etVitorias.text.toString()
+
+        if (nomeDoInvocador == "") {
+            adicionaTexto("Nome de invocador inválido")
+        }
+        if (elo == "") {
+            adicionaTexto("Elo inválido")
+        }
+        if (divisao == "") {
+            adicionaTexto("Divisão inválida")
+        }
+        if (pdl == "" || !isNumeric(pdl)) {
+            adicionaTexto("PDLs inválidos")
+        }
+        if (campeao == "") {
+            adicionaTexto("Campeão inválido")
+        }
+        if (vitorias == "" || !isNumeric(vitorias)) {
+            adicionaTexto("Vitorias inválidas")
+        }
+        return textoDeErro == ""
+
+    }
+
+    fun navegaParaOutraTela() {
+        val myIntent = Intent(this, PerfilActivity::class.java)
+        myIntent.putExtra("invocador", etInvocador.text.toString())
+        myIntent.putExtra("elo", etElo.text.toString())
+        myIntent.putExtra("divisao", etDivisao.text.toString())
+        myIntent.putExtra("pdl", etPdl.text.toString())
+        myIntent.putExtra("campeao", etCampeao.text.toString())
+        myIntent.putExtra("vitorias", etVitorias.text.toString().toInt())
+        this.startActivity(myIntent)
+    }
+
+    fun isNumeric(str: String): Boolean {
+        return try {
+            str.toDouble()
+            true
+        } catch (e: NumberFormatException) {
+            false
+        }
     }
 }
