12d630cda045ae12585291c8e2e7e6af06231044
Victorio <vpn.cash@gmail.com>
2022-05-13 16:39:17 -0300

    Projeto atualizado 13/05

--- app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
+++ app/src/main/java/br/com/estudos/lolzinho/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.estudos.lolzinho
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -12,12 +13,35 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var btnEntrar: Button
     lateinit var etInvocador: TextView
-    lateinit var spElo: Spinner
-    lateinit var etDivisao: TextView
     lateinit var etPdl: TextView
     lateinit var etCampeao: TextView
     lateinit var etVitorias: TextView
-    var textoDeErro: String = ""
+    val listaHighElo: Array<String> = arrayOf(
+        "-"
+    )
+    var listaDivisao: Array<String> = arrayOf(
+        "I",
+        "II",
+        "III",
+        "IV"
+    )
+    val listaElo: Array<String> = arrayOf(
+        TEXTO_FERRO,
+        TEXTO_BRONZE,
+        TEXTO_PRATA,
+        TEXTO_OURO,
+        TEXTO_PLATINA,
+        TEXTO_DIAMENTE,
+        TEXTO_MESTRE,
+        TEXTO_GMESTRE,
+        TEXTO_DESAFIANTE
+    )
+    lateinit var spElo: Spinner
+    lateinit var spDivisao: Spinner
+    var textoDeErro = ""
+    var eloSelecionado = ""
+    var divisaoSelecionada = ""
+    var listaDivisaoSelecionada = listaDivisao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         btnEntrar = findViewById(R.id.bt_perfil)
         etInvocador = findViewById(R.id.et_invocador)
         spElo = findViewById(R.id.sp_elo)
-        etDivisao = findViewById(R.id.et_divisao)
+        spDivisao = findViewById(R.id.sp_divisao)
         etPdl = findViewById(R.id.et_pdl)
         etCampeao = findViewById(R.id.et_campeao)
         etVitorias = findViewById(R.id.et_vitorias)
@@ -43,13 +67,45 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        spElo.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayOf(
-            "Ferro", "Bronze",
-            "Prata", "Ouro",
-            "Platina", "Diamante",
-            "Mestre", "Grão-Mestre",
-            "Desafiante"
-        ))
+        spElo.adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item, listaElo
+        )
+
+        spDivisao.adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item, listaDivisaoSelecionada
+        )
+
+        spElo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                Log.i("teste", "${listaElo[position]}")
+                eloSelecionado = listaElo[position]
+                if (listaElo[position] == TEXTO_MESTRE ||
+                    listaElo[position] == TEXTO_GMESTRE ||
+                    listaElo[position] == TEXTO_DESAFIANTE
+                ) {
+                    listaDivisaoSelecionada = listaHighElo
+                } else {
+                    listaDivisaoSelecionada = listaDivisao
+                }
+                spDivisao.adapter = ArrayAdapter(
+                    this@MainActivity, android.R.layout.simple_spinner_item, listaDivisaoSelecionada
+                )
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
+        spDivisao.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                Log.i("teste", "${listaDivisaoSelecionada[position]}")
+                divisaoSelecionada = listaDivisaoSelecionada[position]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
     }
 
     fun adicionaTexto(param: String) {
@@ -64,17 +120,12 @@ class MainActivity : AppCompatActivity() {
 
         textoDeErro = ""
         var nomeDoInvocador = etInvocador.text.toString()
-        var divisao = etDivisao.text.toString()
         var pdl = etPdl.text.toString()
         var campeao = etCampeao.text.toString()
         var vitorias = etVitorias.text.toString()
 
         if (nomeDoInvocador == "") {
             adicionaTexto("Nome de invocador inválido")
-        }
-
-        if (divisao == "") {
-            adicionaTexto("Divisão inválida")
         }
         if (pdl == "" || !isNumeric(pdl)) {
             adicionaTexto("PDLs inválidos")
@@ -92,12 +143,10 @@ class MainActivity : AppCompatActivity() {
     fun navegaParaOutraTela() {
         val nomeDoInvocador = etInvocador.text.toString()
         val nomeDoCampeaoMaisJogado = etCampeao.text.toString()
-        val nomeDaDivisao = etDivisao.text.toString()
         val quantidadeDePdls = etPdl.text.toString().toInt()
         val quantidadeDeVitorias = etVitorias.text.toString().toFloat()
-        val elo = Elo("", nomeDaDivisao, quantidadeDePdls, quantidadeDeVitorias)
+        val elo = Elo(eloSelecionado, divisaoSelecionada, quantidadeDePdls, quantidadeDeVitorias)
         val invocador = Invocador(nomeDoInvocador, nomeDoCampeaoMaisJogado, elo)
-
         val myIntent = Intent(this, PerfilActivity::class.java)
         myIntent.putExtra("invocador", invocador)
         this.startActivity(myIntent)
